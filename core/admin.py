246c4c9275b611e476ed7b941bcbd92e308e2d85
Jonatas Ezequiel de Oliveira <jonatas.eoliveira@gmail.com>
2020-02-28 15:14:11 -0400

    Adding Update and Delete functions

--- core/admin.py
+++ core/admin.py
@@ -5,5 +5,5 @@ from core.models import Evento
 
 @admin.register(Evento)
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao')
     list_filter = ('titulo',)
