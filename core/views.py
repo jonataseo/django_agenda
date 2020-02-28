742fccfaeb47536fadcb62dff65b7ec13731af6f
Jonatas Ezequiel de Oliveira <jonatas.eoliveira@gmail.com>
2020-02-28 11:33:53 -0400

    Created event list view page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from core.models import Evento
 
 # Create your views here.
+def retorna_descricao(request, titulo_evento):
+    descricao = Evento.objects.get('titulo' == titulo_evento).descricao
+    return HttpResponse("Local do evento: {}".format(descricao))
+#
+# def index(request):
+#     return redirect('/agenda/')
+
+def lista_eventos(request):
+    evento = Evento.objects.all()
+    response = {'eventos': evento}
+    return render(request,'agenda.html', response)
