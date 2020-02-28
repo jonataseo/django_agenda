c5a5b66c0066713431fadf643fb689a081b4168b
Jonatas Ezequiel de Oliveira <jonatas.eoliveira@gmail.com>
2020-02-28 13:55:01 -0400

    Included user authentication (Login and Logout)

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def retorna_descricao(request, titulo_evento):
@@ -9,7 +12,28 @@ def retorna_descricao(request, titulo_evento):
 # def index(request):
 #     return redirect('/agenda/')
 
+@login_required(login_url='/login')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     response = {'eventos': evento}
     return render(request,'agenda.html', response)
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuario ou senha invalido")
+    return redirect('/')
